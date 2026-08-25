663c983038124783161e72b2068cf0d0e9ae4e72
Enguerrand Bourghelle <enguerrand.bourghelle.agence@gmail.com>
2026-08-25 10:55:12 +0200

    Tarifs : création 1000€ + maintenance 89€/mois

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Stats from "../components/Stats";
 import Services from "../components/Services";
 import APropos from "../components/APropos";
 import Portfolio from "../components/Portfolio";
+import Tarifs from "../components/Tarifs";
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
 
@@ -28,6 +29,7 @@ export default function Home() {
       <Services />
       <APropos />
       <Portfolio />
+      <Tarifs />
       <Contact />
       <Footer />
     </main>
